a2dda1a78804e89e016806822df25cf97166282d
PunPunPun13 <18823369302@163.com>
2019-03-20 01:45:45 +0800

    ex_and_text my homework

--- python_text-master/ex_text/b10-面向对象编程.py
+++ python_text-master/ex_text/b10-面向对象编程.py
@@ -65,3 +65,94 @@ class DictClass(object):
 
 d = DictClass({"a": 1, "b": 2})
 print(d.update_dict({"c": 3}))
+
+
+# 定义一个列表的操作类 ListInfo 包括的方法
+
+# 1.列表元素添加:add_key()  添加的必须是数字或者字符串
+# 2.列表元素取值:get_key()
+# 3.列表合并:update_list(list)
+# 4.删除并且返回最后一个元素:del_key()
+
+class ListInfo(object):
+
+    def __init__(self, list_val):
+        self.list = list_val
+
+    def add_key(self, key_name):
+        # 添加的key必须是字符串或者是数字
+        if isinstance(key_name, (str, int)):
+            self.list.append(key_name)
+            print(self.list)
+            return "Ok"
+        else:
+            return "我要字符串或者数字"
+
+    def get_key(self, index):
+        # 判断传入的索引是否超出了列表
+        if index >= 0 and index < len(self.list):
+            return self.list[index]
+        return "你输入的太多了,想太多了"
+
+    def update_list(self, new_list):
+        self.list.extend(new_list)  # 先让对象new_list合并一下
+        return self.list  # 重新执行list, return完整的列表
+
+    def del_key(self):
+        # 首先要判断我们的列表里面是否还有元素
+        if len(self.list) >= 0:
+            return self.list.pop(-1)  # 弹出最后一个元素
+        else:
+            return "列表是空的"
+
+
+list_info = ListInfo([1, 2, 3, 4, 5])
+
+print(list_info.add_key([1, 4, 5, 6]))
+# print(list_info.get_key(3))
+# print(list_info.update_list([8,9,10]))
+# print(list_info.del_key())
+
+
+
+# 定义一个集合的操作类
+
+# 包括的方法
+
+# 1.集合元素添加: add_setinfo()
+# 2.集合的交集: get_intersection()
+# 3.集合的并集: get_union()
+# 4.集合的差集: del_difference()
+
+class SecInfo(object):
+    def __init__(self, my_set):
+        self.sett = my_set
+
+    def add_setinfo(self, keyname):
+        self.sett.add(keyname)
+        return self.sett
+
+    def get_intersection(self, unioninfo):
+        if isinstance(unioninfo, set):
+            return self.sett & unioninfo
+        else:
+            return "你传入的不是set"
+
+    def get_union(self, unioninfo):
+        if isinstance(unioninfo, set):
+            return self.sett | unioninfo
+        return "你传入的不是set"
+
+    def del_difference(self, unioninfo):
+        if isinstance(unioninfo, set):
+            return self.sett - unioninfo
+        return "你传入的不是set"
+
+
+A = set([1, 2, 3, 4, 5])
+B = set([5, 6, 3])
+my_set = SecInfo((A))
+# print(my_set.add_setinfo(6))
+# print(my_set.get_union(B))
+# print(my_set.del_difference(B))
+print(my_set.del_difference("123"))
